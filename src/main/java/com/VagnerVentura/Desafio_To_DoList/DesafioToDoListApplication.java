971251b87d86473bb0b4eceb3d5a335babd78194
vagner ventura <vagnerventura@hotmail.com>
2024-07-29 21:00:50 -0300

    Inicializando o Projeto

---
+++ src/main/java/com/VagnerVentura/Desafio_To_DoList/DesafioToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.VagnerVentura.Desafio_To_DoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VagnerVentura/Desafio_To_DoList/DesafioToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VagnerVentura.Desafio_To_DoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
